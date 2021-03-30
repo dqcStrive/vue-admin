b1235149d819c39235d0d8c59b4b368b32c60340
请叫我帅灿 <1447119622@qq.com>
2021-03-30 13:24:40 +0800

    构建项目/配置全局样式

---
+++ src/views/Login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="login">
+    this is login
+    <el-button type="">默认按钮</el-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+ #login{
+   height: 100vh;
+   background-color: #344a5f;
+ }
+</style>
